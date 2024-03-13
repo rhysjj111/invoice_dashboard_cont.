392fe94115e423cdd9551b6a7d1f9c6c876eb80f
Rhys Johnston <rhysjj111@gmail.com>
2024-03-13 16:11:42 +0000

    Add invoice_summary.html. Insert bootstrap collapse for invoice details, parts, labour and totals. Link urls and create a temporary view for the new page.

--- invoice/urls.py
+++ invoice/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.invoice_list, name='invoice_list'),
+    path('invoice', views.invoice, name='invoice'),
 ]
--- invoice/views.py
+++ invoice/views.py
@@ -9,3 +9,14 @@ def invoice_list(request):
         'delete_url': delete_url,
     }
     return render(request, 'invoice/invoice_list.html', context)
+
+
+def invoice(request):
+    """A view to return a single-invoice view"""
+    previous_url = 'invoice_list'
+    delete_url = 'invoice_list'
+    context = {
+        'previous_url': 'invoice_list',
+        'delete_url': delete_url,
+    }
+    return render(request, 'invoice/invoice.html', context)
