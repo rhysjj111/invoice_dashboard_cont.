7a28dab264998d3355e0c5b60397244c4045809c
Rhys Johnston <rhysjj111@gmail.com>
2024-04-04 10:08:43 +0000

    Update templates to express part.total instead of part.subtotal to account for previous change. Update invoice app admin.py to include custom Admin model for Invoice with inline entries for labour and part. Add function to format date in to InvoiceAdmin class.

--- invoice/admin.py
+++ invoice/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
 from .models import Invoice, Part, Labour
+from django.utils.dateformat import format
 
+class PartAdminInline(admin.TabularInline):
+    model = Part
+    readonly_fields = ('total',)
+
+class LabourAdminInline(admin.TabularInline):
+    model = Labour
+    readonly_fields = ('total',)
+
+class InvoiceAdmin(admin.ModelAdmin):
+    inlines = (PartAdminInline, LabourAdminInline)
+
+    readonly_fields = ('inv_number', 'inv_integer', 'parts_total',
+                       'labour_total', 'subtotal', 'vat_total',
+                       'grand_total', 'slug', 'unique_id',)
+    
+    list_display = ('__str__', 'formatted_date_in', 'grand_total',)
+
+    def formatted_date_in(self, obj):
+        return format(obj.date_in, 'd/m/Y')
+
+    formatted_date_in.short_description = 'Date In'
+    ordering = ('-date_in',)
+    
 # Register your models here.
-admin.site.register(Invoice)
-admin.site.register(Part)
-admin.site.register(Labour)
+admin.site.register(Invoice, InvoiceAdmin)
--- invoice/signals.py
+++ invoice/signals.py
@@ -9,7 +9,7 @@ def update_on_parts_save(sender, instance, created, **kwargs):
     """
     Update invoice totals upon part add/edit
     """
-    instance.invoice.update_total(request=request)
+    instance.invoice.update_total()
 
 
 @receiver(post_delete, sender=Part)
@@ -17,7 +17,7 @@ def update_on_parts_delete(sender, instance, **kwargs):
     """
     Update invoice totals upon part delete
     """
-    instance.invoice.update_total(request=request)
+    instance.invoice.update_total()
 
 #labour signals
 @receiver(post_save, sender=Labour)
@@ -25,11 +25,11 @@ def update_on_labour_save(sender, instance, created, **kwargs):
     """
     Update invoice totals upon labour add/edit
     """
-    instance.invoice.update_total(request=request)
+    instance.invoice.update_total()
 
 @receiver(post_delete, sender=Labour)
 def update_on_labour_delete(sender, instance, **kwargs):
     """
     Update invoice totals upon labour delete
     """
-    instance.invoice.update_total(request=request)
+    instance.invoice.update_total()
