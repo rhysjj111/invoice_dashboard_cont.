7d660b489a9c4e7b12953c72f6d59c734fd0646f
Rhys Johnston <rhysjj111@gmail.com>
2024-04-04 10:12:56 +0000

    Update customer app admin.py to make customer and vehicle slug fields readonly.

--- customer/admin.py
+++ customer/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Customer, Vehicle
 
+class CustomerAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('slug',)
+
+class VehicleAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('slug',)
+
+
 # Register your models here.
-admin.site.register(Customer)
-admin.site.register(Vehicle)
+admin.site.register(
+    Customer, CustomerAdmin)
+admin.site.register(Vehicle, VehicleAdmin)
