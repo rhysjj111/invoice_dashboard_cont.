9252c16681fba1b47251a833cc4f0b4185e1ff30
Rhys Johnston <rhysjj111@gmail.com>
2024-04-05 23:08:52 +0000

    Move messages into toasts. Format toasts to have a different header, depending on whether success or warning message.

--- static/js/base.js
+++ static/js/base.js
@@ -1,6 +1,14 @@
 const currentUrl = window.location.href;
+//initialize toasts
 
 document.addEventListener('DOMContentLoaded', function() {
+
+  // Initialize toasts
+  const toastElList = document.querySelectorAll('.toast');
+  const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl, { autohide: true, delay: 10000 })); // Set delay to 5000 milliseconds (5 seconds)
+
+  // Display toasts
+  toastList.forEach(toast => toast.show());
   
   if (currentUrl.includes("invoice_select")){
     hideLastDeleteInput('parts')
