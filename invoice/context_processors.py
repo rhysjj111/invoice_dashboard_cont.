6b19d05d1e14c3ffa51532bf49705f30b03cd6a7
Rhys Johnston <rhysjj111@gmail.com>
2024-04-06 02:00:41 +0000

    No idea what was wrong but Heroku app working now

--- invoice/context_processors.py
+++ invoice/context_processors.py
@@ -3,6 +3,5 @@ def user_creds(request):
         'Mechanic': request.user.groups.filter(name='Mechanic').exists(),
         'Accounts': request.user.groups.filter(name='Accounts').exists(),
         'Foreman': request.user.groups.filter(name='Foreman').exists(),
-        'test': request.user,
     }
 
