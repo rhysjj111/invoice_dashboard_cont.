9408eba05785f3dd1e8a08ae441c3365c075b001
Rhys Johnston <rhysjj111@gmail.com>
2024-04-06 01:15:19 +0000

    testing context processor is working

--- invoice/context_processors.py
+++ invoice/context_processors.py
@@ -3,5 +3,6 @@ def user_creds(request):
         'Mechanic': request.user.groups.filter(name='Mechanic').exists(),
         'Accounts': request.user.groups.filter(name='Accounts').exists(),
         'Foreman': request.user.groups.filter(name='Foreman').exists(),
+        'test': 'salkdjfaskldjf',
     }
 
